0ffb14bad4c152ce47a41930c1ec01f032bdd048
Jahid2121 <138579420+Jahid2121@users.noreply.github.com>
2024-06-08 07:03:32 +0600

    Create component for adding todo and also another compoent for showing all todos

--- src/components/TodoItem.tsx
+++ src/components/TodoItem.tsx
@@ -1,11 +1,27 @@
-import React from 'react';
-
-const TodoItem = () => {
+interface ItemTodo {
+    todo: any;
+    editTodoItem: FunctionStringCallback;
+    deleteTodoItem: FunctionStringCallback;
+  }
+  function TodoItem({ todo, editTodoItem, deleteTodoItem }: ItemTodo) {
     return (
-        <div>
-            TODO Item
+      <>
+        <div className="todoItem">
+          <div>{todo.attributes.todoText}</div>
+          <div>
+            <i>
+              <button className="bg-default" onClick={() => editTodoItem(todo)}>
+                Edit
+              </button>
+            </i>
+            <i>
+              <button className="bg-danger" onClick={() => deleteTodoItem(todo)}>
+                Del
+              </button>
+            </i>
+          </div>
         </div>
+      </>
     );
-};
-
-export default TodoItem;
+  }
+  export default TodoItem;
