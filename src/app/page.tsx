bef731976cede4b6754e0dd98fbcc64610bd6c83
Jahid2121 <138579420+Jahid2121@users.noreply.github.com>
2024-06-08 07:19:19 +0600

    Create Query using graphql to fetch todo data

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,26 @@
-"use client"
-
+"use client";
+import AddTodo from "@/containers/AddTodo";
+import TodoList from "@/containers/TodoList";
+import { useEffect, useState } from "react";
 export default function Home() {
+  const [todos, setTodos] = useState<[]>([]);
+  const addTodo = async (todoText: string) => {};
+  const editTodoItem = async (todo: any) => {
+    console.log("Edited");
+  };
+  const deleteTodoItem = async (todo: any) => {
+    console.log("Deleted");
+  };
   return (
-   <div>
-   </div>
+    <div>
+      <main className="main">
+        <AddTodo addTodo={addTodo} />
+        <TodoList
+          todos={todos}
+          deleteTodoItem={deleteTodoItem}
+          editTodoItem={editTodoItem}
+        />
+      </main>
+    </div>
   );
 }
