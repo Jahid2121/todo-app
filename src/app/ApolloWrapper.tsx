8eebe285e30eeefd4618d7da9514b4c7300da6dc
Jahid2121 <138579420+Jahid2121@users.noreply.github.com>
2024-06-10 10:56:00 +0600

    Add Type for children props passed in apollowrapper

--- src/app/ApolloWrapper.tsx
+++ src/app/ApolloWrapper.tsx
@@ -1,8 +1,14 @@
 "use client"
 import client from '@/lib/client';
 import { ApolloProvider } from '@apollo/client';
+import { ReactNode } from 'react';
 
-const ApolloProviderWrapper = ({ children }) => {
+interface ApolloProviderWrapperProps {
+  children: ReactNode;
+}
+
+
+const ApolloProviderWrapper: React.FC<ApolloProviderWrapperProps> = ({ children }) => {
   return (
     <ApolloProvider client={client}>
       {children}
